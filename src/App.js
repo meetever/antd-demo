ac1410a04ce20a813eeeb133c0c5992f3f626a18
Christoph Schramm <schramm@makaio.com>
2020-12-16 14:07:47 +0100

    before eject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+import { Typography, Layout, Space } from 'antd';
 
-function App() {
+import './App.less';
+const { Header, Footer, Sider, Content } = Layout;
+const { Text } = Typography;
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/grundlayout">Grundlayout</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            <Route path="/grundlayout">
+              <Grundlayout />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Grundlayout() {
+  return (
+      <Layout>
+        <Header>
+          <Text type="warning">Ant Design (warning)</Text>
+        </Header>
+        <Layout>
+          <Sider>
+            <Text type="success">Ant Design (success)</Text>
+          </Sider>
+          <Content>
+            <Space direction="vertical">
+              <Text type="secondary">Ant Design (secondary)</Text>
+            </Space>
+          </Content>
+        </Layout>
+        <Footer>Footer</Footer>
+      </Layout>
+  );
+}
